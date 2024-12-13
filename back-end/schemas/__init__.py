8052f454444cdc4d8638f598e40411c2bb38c4cb
Pedro Pinto Moya <60751786+pedropintomoya@users.noreply.github.com>
2024-12-13 18:48:49 -0300

    Delete back-end directory

--- back-end/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from schemas.comentario import ComentarioSchemaProdutor, ComentarioSchemaFornecedor, ComentarioSchemaEvento
-from schemas.produtor import ProdutorSchema
-from schemas.error import ErrorSchema
-from schemas.fornecedor import FornecedorSchema
-from schemas.evento import EventoSchema
